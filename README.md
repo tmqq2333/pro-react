952846e9d53eddae103135d0fe31005da351d7bc
tmqq2333 <tmqq2333@163.com>
2023-03-07 17:00:25 +0800

    antd5主题切换

--- README.md
+++ README.md
@@ -7,3 +7,26 @@
 123456    游客权限  
 000000    管理员权限     
 账号等于昵称。可自行设置效验
+
+#### 用到react相关的生态链模块:
+* `react v18.2.0`
+* `react-router-dom v6.8.2`
+* `react-redux v8.0.5`
+* `axios v1.3.4`
+* `node v16.14.0`
+
+#### 用到其他库和第三方包:
+* `antd 5.3.0`
+* `echarts 5.4.1`
+* `axios 1.3.4`
+* `react-antd-select 1.0.7`  
+
+
+### 项目启动步骤
+1. 安装包
+   npm install
+   `如需 切换淘宝镜像 npm i  --registry https://registry.npm.taobao.org`
+2. 开发运行
+   npm run dev:dev
+3. 生产打包
+   npm run build:prod
--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import BaseRouter from "./router";
 import "./assets/base.scss";
 import { Provider } from "react-redux";
 import store from "./store/index";
-
+import { ConfigProvider } from "antd";
+import zhCN from "antd/lib/locale/zh_CN";
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <BaseRouter></BaseRouter>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        token: {
+          colorPrimary: "#20d18b",
+        },
+      }}
+    >
+      <BaseRouter></BaseRouter>
+    </ConfigProvider>
   </Provider>
 );
